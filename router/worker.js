6ee4855d872ea8ec2cf2056149a22be30081fad1
kuiper <kuiper@kuiper.dog>
2023-07-21 00:43:42 -0700

    Add initial Carrd support

--- router/worker.js
+++ router/worker.js
@@ -1,5 +1,22 @@
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('wav.haus');
+		const url = new URL(request.url);
+		const config = env.kv.get(url.hostname.split('.').reverse()[2]);
+
+		if (!config || !config.settings)
+			return new Response('https://wav.haus', 302)
+		
+		switch (config.settings.type) {
+			case "carrd":
+				const content = await fetch(config.settings.url + url.pathname + url.search + url.hash);
+				// Appends the original host to relative asset paths to reduce additional requests:
+				var html = (await content.text()).replace(/(src|href)=["']((?!http|\/\/|#)[^"']+)/g, `$1="${config.settings.url}/$2`);
+				if (config.settings.nofooter)
+					html = html.replace(/<[^>]*id\s*=\s*["']credits["'][^>]*>.*?<\/[^>]*>/g, '');
+				return new Response(html, {headers: content.headers});
+
+		}
+
+		return new Response();
 	},
 };
