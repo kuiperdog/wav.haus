926789fb6587f1747ebf5df2639f662cd2816060
kuiper <kuiper@kuiper.dog>
2023-07-28 11:28:44 -0700

    Don't append host to JavaScript HREFs

--- router/worker.js
+++ router/worker.js
@@ -13,7 +13,7 @@ export default {
 				const content = await fetch(config.settings.url + url.pathname + url.search + url.hash);
 				// Appends the original host to relative asset paths to reduce additional requests:
 				if (content.headers.get('Content-Type').includes('text/html')) {
-					var html = (await content.text()).replace(/(src|href)=["']((?!http|\/\/|#)[^"']+)/g, `$1="${config.settings.url}/$2`);
+					var html = (await content.text()).replace(/(src|href)=["']((?!(http|\/\/|#|javascript:))[^"']+)/g, `$1="${config.settings.url}/$2`);
 					if (config.settings.nofooter)
 						html = html.replace(/<[^>]*id\s*=\s*["']credits["'][^>]*>.*?<\/[^>]*>/g, '');
 					return new Response(html, {headers: content.headers});
